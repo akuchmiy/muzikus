e6eb393a41ea06b8bbb582a78b9fb12c16f9e68d
Anton Kuchmii <a.kuchmiy19@gmail.com>
2021-12-25 17:28:34 +0200

    prettier, favicon

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  bracketSpacing: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  semi: false,
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  printWidth: 80,
+  arrowParens: 'always',
+}
